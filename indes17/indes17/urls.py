7c8148826855aaab2a1221fdf5bb2328892d4c92
dsaxen <da_sax_e@hotmail.com>
2017-12-01 17:58:00 +0200

    Add files via upload

---
+++ indes17/indes17/urls.py
@@ -0,0 +1,15 @@
+from indesApp.views import *
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', RequestWeather.as_view(), name="RequestWeather"),
+    url(r'^favorites$', favorites),
+    url(r'^profile$', profile),
+    url(r'^faq$', faq),
+    url(r'^language$', language),
+    url(r'^register$', register),
+    url(r'^login$', login),
+    
+]
